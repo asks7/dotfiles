a43b88d224f0b1bd5ba0edd2865a9d7ea38fca9d
okarin <haru_s2@live.jp>
2015-04-27 17:09:21 +0900

    dotfiles

---
+++ firefox/user.js
@@ -0,0 +1,22 @@
+
+user_pref('dom.ipc.plugins.enabled', false);
+user_pref('geo.enabled', false);
+user_pref('ui.submenuDelay', 0);
+user_pref('ui.use_unity_menubar', false);
+user_pref('nglayout.initialpaint.delay', 100);
+user_pref('accessibility.blockautorefresh', true);
+
+user_pref('network.dns.disableIPv6', true);
+
+user_pref('browser.tabs.animate', false);
+"user_pref('browser.download.manager.retention', 120000);
+user_pref('browser.panorama.animate_zoom', false);
+user_pref('browser.enable_automatic_image_resizing', false);
+user_pref('browser.tabs.animate', 1);
+"user_pref('browser.cache.memory.enable', true);
+"user_pref('browser.cache.memory.max_entry_size', 1);
+user_pref('browser.cache.disk.enable', false);
+user_pref('browser.cache.disk.max_entry_size', 1);
+"user_pref('network.http.pipelining', true);
+"user_pref('network.http.proxy.pipelining', true);
+
